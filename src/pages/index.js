4d06c661b3a63938323ef43ed5a5bbbab14ab6e5
paulayuk <ayukpaul1@gmail.com>
2019-07-27 07:33:10 +0100

    removed unecessary files and comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import Posts from '../components/Posts';
 
 
